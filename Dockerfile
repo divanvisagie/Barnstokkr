558d8e96402198c8d5ba4542fc327ee1a7bdcf39
Divan visagie <me@dvisagie.com>
2024-03-17 00:03:06 +0100

    Update to be lightweight on raspi

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # Use an official Python runtime as a parent image
-FROM python:3.8-slim
+FROM python:latest
 
 # Set the working directory in the container
 WORKDIR /usr/src/app
@@ -10,12 +10,8 @@ COPY . .
 
 # Install any needed packages specified in requirements.txt
 
-
 # Make port 8000 available to the world outside this container
 EXPOSE 8000
-
-# Define environment variable
-ENV NAME World
 
 # Run app.py when the container launches
 CMD ["uvicorn", "app:app", "--host", "0.0.0.0", "--reload"]
--- app.py
+++ app.py
@@ -1,16 +1,10 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-from transformers import AutoTokenizer, AutoModel, AutoModelForCausalLM
+from transformers import AutoTokenizer, AutoModel
 from typing import List
-import os
 import torch
 
 app = FastAPI()
-
-# Load pre-trained model and tokenizer
-embeddings_model = "distilbert-base-uncased"
-e_tokenizer = AutoTokenizer.from_pretrained(embeddings_model)
-e_model = AutoModel.from_pretrained(embeddings_model)
 
 
 class EmbeddingsRequest(BaseModel):
@@ -29,6 +23,10 @@ def read_root():
 @app.post("/embeddings", response_model=EmbeddingsResponse)
 async def create_embeddings(item: EmbeddingsRequest):
     try:
+        # Load pre-trained model and tokenizer
+        embeddings_model = "distilbert-base-uncased"
+        e_tokenizer = AutoTokenizer.from_pretrained(embeddings_model)
+        e_model = AutoModel.from_pretrained(embeddings_model)
         # Encode text
         inputs = e_tokenizer(item.text, return_tensors="pt",
                              padding=True, truncation=True, max_length=512)
@@ -39,35 +37,5 @@ async def create_embeddings(item: EmbeddingsRequest):
         # then take the mean of the sequence dimension
         embeddings = outputs.last_hidden_state.mean(dim=1).tolist()[0]
         return {"embeddings": embeddings}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-
-
-# Set your Hugging Face token
-huggingface_token = os.environ.get("HUGGINGFACE_TOKEN")
-# Load conversational model and tokenizer
-model_name = "google/gemma-2b"
-tokenizer = AutoTokenizer.from_pretrained(
-    model_name, use_auth_token=huggingface_token)
-model = AutoModelForCausalLM.from_pretrained(
-    model_name, use_auth_token=huggingface_token, device_map="auto")
-
-
-class ChatRequest(BaseModel):
-    message: str
-
-
-class ChatResponse(BaseModel):
-    reply: str
-
-
-@app.post("/chat", response_model=ChatResponse)
-async def chat(item: ChatRequest):
-    try:
-        input_ids = tokenizer(item.message, return_tensors="pt")#.to("cuda")
-
-        outputs = model.generate(**input_ids)
-        reply = tokenizer.decode(outputs[0])
-        return {"reply": reply}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
